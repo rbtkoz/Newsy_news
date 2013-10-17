b358a52d58d5516e329ae40b893180701169a0a7
AK <kozovski.a@gmail.com>
2013-10-17 19:42:03 -0400

    link to story's show page

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,9 @@
+class StoriesController <ApplicationController
+
+  def show
+
+    @story = Story.find(params[:id])
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ NewsyNews::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  
+  resources :stories, :only =>[:show]
+  #show me only
   root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
