3444ea2d18f3f6454b9fe3a20e8e413b74632367
AK <kozovski.a@gmail.com>
2013-10-17 19:05:27 -0400

    brand new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewsyNews::Application.config.secret_key_base = 'c1f57bcf4d1b9d09c65c78585be936725832cec8b3c204569ec84622e7fcd4273d0cc64599c5d37cb767111473261af02bcadefb27f3f9c8e9ecd3e70b8b4de6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewsyNews::Application.config.session_store :cookie_store, key: '_newsy_news_session'
